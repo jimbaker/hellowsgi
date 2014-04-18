33073d94abbb359b83ad31b302deddd6f7b8b146
Jim Baker <jim.baker@rackspace.com>
2014-04-17 21:39:36 -0600

    wsgiref.validate

--- hellowsgi/__init__.py
+++ hellowsgi/__init__.py
@@ -1,7 +1,11 @@
+from wsgiref.validate import validator
+
+@validator
 def simple_app(environ, start_response):
     """Simplest possible application object"""
     status = '200 OK'
-    output = b'Hello World. Calling in via Fireside!\n'
+    output = b"Hello World - let's use Fireside!\n"
     response_headers = [('Content-type', 'text/plain')]
     start_response(status, response_headers)
     return [output]
+
